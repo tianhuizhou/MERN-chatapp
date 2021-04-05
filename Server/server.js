a815003ecd43070cadb9d362dbda0ae37aa90305
tianhuizhou <tianhui@hawaii.edu>
2021-04-04 20:57:59 -1000

    initialize a simple server for testing

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+//importing
+import express from 'express';
+
+//app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+
+//DB config
+
+//
+
+//api routes
+app.get('/', ((req, res) => res.status(200).send('hello world!')))
+
+//listen
+app.listen(port, () =>console.log(`Listening on localhost:${port}`));
